1b4a8e749502589e418a3d83b1b0c7d130eca177
Cordell Andersen <abysalstar@aol.com>
2018-02-20 14:44:33 -0700

    add Global and AppConfig from YogoGit

---
+++ app/Global.java
@@ -0,0 +1,22 @@
+import configs.AppConfig;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+ 
+import play.Application;
+import play.GlobalSettings;
+ 
+public class Global extends GlobalSettings {
+    private ApplicationContext ctx;
+ 
+    @Override
+    public void onStart(Application app) {
+        super.onStart(app);
+        ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+    }
+
+    @Override
+    public <A> A getControllerInstance(Class<A> clazz) {
+        return ctx.getBean(clazz);
+    }
+}
